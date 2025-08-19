225672dd67acc23078664ad0eb296a9c4a2d4efe
Augusto <160799418+augustocaixeta@users.noreply.github.com>
2025-08-19 19:46:51 -0300

    .

--- sort/bubble-sort.py
+++ sort/bubble-sort.py
@@ -1,9 +1,10 @@
-arr = [5, 4, 3, 2, 1]
-len = len(arr) - 1
+def bubble_sort(arr):
+    n = len(arr) - 1
+    for step in range(n):
+        for i in range(n - step):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
 
-for steps in range(len):
-    for i in range(len - steps):
-        if arr[i] > arr[i+1]:
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-
+arr = [6, 7, 9, 4, 3, 0]
+bubble_sort(arr)
 print(arr)
