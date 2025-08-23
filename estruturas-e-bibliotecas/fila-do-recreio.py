cb0788d77ac9ea0564267ab926562efe6ac8124e
Augusto <160799418+augustocaixeta@users.noreply.github.com>
2025-08-22 22:10:57 -0300

    .

---
+++ estruturas-e-bibliotecas/fila-do-recreio.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+while n:
+    m = int(input())
+    arr = list(map(int, input().split()))
+    oth = arr[:]
+
+    arr.sort(reverse=True)
+    unch = sum(1 for i in range(m) if arr[i] == oth[i])
+
+    print(unch)
+    n -= 1
