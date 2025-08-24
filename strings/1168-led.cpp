c94bd258b25ca3552a459f213c26d70fbc5a292d
Augusto <160799418+augustocaixeta@users.noreply.github.com>
2025-08-24 16:38:09 -0300

    .

--- strings/1168-led.cpp
+++ strings/1168-led.cpp
@@ -7,14 +7,12 @@ int main() {
     int const arr[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 
     while (N--) {
-        int V;
-        scanf("%d", &V);
+        char str[101];
+        scanf("%s", str);
 
-        int num, leds = 0;
-        while (V) {
-            num = V % 10;
-            leds += arr[num];
-            V /= 10;
+        int i = 0, leds = 0;
+        while (str[i]) {
+            leds += arr[str[i++] - '0'];
         }
 
         printf("%d leds\n", leds);
@@ -22,5 +20,6 @@ int main() {
 
     return 0;
 }
+
 
 // https://judge.beecrowd.com/pt/problems/view/1168
